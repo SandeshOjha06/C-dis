abada75110b36717aae7fabc57d88bcfa6f41926
Sandesh <sandeshojha70@gmail.com>
2026-05-18 11:10:37 +0545

    update hash table

--- src/store.c
+++ src/store.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 #define HASH_SIZE 1024
 
@@ -45,7 +46,7 @@ HashTable *ht_create(void) {
 int ht_set(HashTable *ht, const char *key, const char *val) {
     unsigned int index = hash(key);
     // search bucket chain for existing key first
-    Entry *curr = ht->bucket[index];
+    Entry *curr = ht->buckets[index];
     while (curr != NULL) {
         if (strcmp(curr->key, key) == 0){
             // if found: update value
@@ -55,9 +56,23 @@ int ht_set(HashTable *ht, const char *key, const char *val) {
             
         }
         curr = curr->next;
-        // if not found: create new Entry, prepend to chain
         
     }
+    // if not found: create new Entry, prepend to chain
+    Entry *new_entry = malloc(sizeof(Entry));
+    if (new_entry == NULL) return 0;
+
+    strncpy(new_entry->key,key,sizeof(new_entry->key) - 1);
+    new_entry->key[sizeof(new_entry->key) -1] = '\0'; 
+    strncpy(new_entry->val,val,sizeof(new_entry->val) - 1);
+    new_entry->val[sizeof(new_entry->key) - 1] = '\0';
+
+    // prepend to chain
+    new_entry->next = ht->buckets[index];
+    ht->buckets[index] = new_entry;
+    ht->count++;
+
+    return 1;
 }
     
 // ht_get() - find key, return value pointer or NULL
@@ -70,10 +85,10 @@ int ht_set(HashTable *ht, const char *key, const char *val) {
 // ht_destroy() - free everything
 
 int main(void) {
-    printf("hash(name)     = %u\n", hash("name"));
-    printf("hash(city)     = %u\n", hash("city"));
-    printf("hash(name)     = %u\n", hash("name")); // same as first?
-    printf("hash(aaaa)     = %u\n", hash("aaaa"));
-    printf("hash(bbbb)     = %u\n", hash("bbbb"));
+    HashTable *ht = ht_create();
+    ht_set(ht, "name", "Sandesh");
+    ht_set(ht, "city", "Kathmandu");
+    ht_set(ht, "name", "Ojha");  // update
+    printf("count = %d\n", ht->count);  // must print 2
     return 0;
 }
