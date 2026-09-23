e8c3cb2722d53d3e74d3507d46da88dc96e8326e
Sandesh <sandeshojha70@gmail.com>
2026-09-23 21:54:31 +0530

    feat(main): enhance boot logging and update hostname format

--- src/main.c
+++ src/main.c
@@ -15,6 +15,10 @@ int my_node_id = 0;
 int cluster_size = 1;
 
 int main(void) {
+ //Disable buffering immediately
+  setbuf(stdout, NULL);
+  printf("[boot] Starting C-dis Initialization...\n");
+
   char *env_size = getenv("CLUSTER_SIZE");
   if (env_size != NULL) {
     cluster_size = atoi(env_size);
@@ -26,8 +30,8 @@ int main(void) {
 
   char *hostname = getenv("HOSTNAME");
   if (hostname != NULL) {
-    sscanf(hostname, "kv-store-%d", &my_node_id);
-    printf("Starting node %d of %d\n", my_node_id, cluster_size);
+    sscanf(hostname, "my-database-sts-%d", &my_node_id);
+    printf("[boot] Starting node %d of %d\n", my_node_id, cluster_size);
   }
 
   int server_fd = socket(AF_INET, SOCK_STREAM, 0);
@@ -37,8 +41,7 @@ int main(void) {
   }
 
   int val = 1;
-  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val)) ==
-      -1) {
+  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val)) == -1) {
     perror("setsockopt");
     return 1;
   }
@@ -58,16 +61,21 @@ int main(void) {
     return 1;
   }
 
-  printf("KV store listening on port 6379\n");
+  printf("[boot] KV store listening on port 6379\n");
 
   HashTable *ht = ht_create();
   if (!ht) {
     fprintf(stderr, "ht_create failed\n");
     return 1;
   }
-  g_log_fd = -1;                      // disable logging
-  wal_replay("kvstore.log", ht);      // replay without writing
-  g_log_fd = wal_open("kvstore.log"); // enable logging after
+
+// Write directly to the Kubernetes Persistent Volume
+  const char* wal_path = "/app/data/kvstore.log";
+  g_log_fd = -1;                      
+  wal_replay(wal_path, ht);      
+  g_log_fd = wal_open(wal_path); 
+
+  // Start the network mesh
   server_run_epoll(server_fd, ht);
 
   ht_destroy(ht);
